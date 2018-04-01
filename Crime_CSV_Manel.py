2ef3aff094eca9e6a3e0727b047a9ba67359e1a1
manelmahroug <manel.khan@gmail.com>
2018-04-01 11:44:33 -0700

    Delete Crime_CSV_Manel.py

--- Crime_CSV_Manel.py
+++
@@ -1,5 +0,0 @@
-Python 3.6.0b2 (v3.6.0b2:b9fadc7d1c3f, Oct 10 2016, 13:36:53) 
-[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
-Type "copyright", "credits" or "license()" for more information.
->>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
-Visit http://www.python.org/download/mac/tcltk/ for current information.
